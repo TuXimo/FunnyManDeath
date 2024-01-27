8f2945357b842f1f0e28332a0ccd1cf37ff9ba88
Máximo Brunetti <67173706+TuXimo@users.noreply.github.com>
2024-01-27 12:28:31 -0300

    dialogo

--- Assets/Scripts/DialogueScript.cs
+++ Assets/Scripts/DialogueScript.cs
@@ -2,39 +2,40 @@
 using Scriptable_Objects;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class DialogueScript : MonoBehaviour
 {
     [SerializeField] private DialogueAsset dialogues;
     [SerializeField] private TMP_Text _tmpText;
-    [SerializeField] private Button _button;
+    [SerializeField] private Button _nextButton;
 
-    private int _textIndex = 0;
+    private int _textIndex;
     
     private void OnEnable()
     {
         SetText(0);
     }
-
+    
     private void Start()
     {
-        _button.onClick.AddListener(NextText);
+        _nextButton.onClick.AddListener(NextText);
     }
 
 
-    private void NextText()
+    public void NextText()
     {
         _textIndex++;
            
         if (_textIndex < dialogues.dialogue.Length)
-        {
+        {   
             SetText(_textIndex);
         }
         
         else
         {
-            gameObject.SetActive(false);
+            Destroy(gameObject);
         }
     }
 
@@ -44,7 +45,5 @@ public class DialogueScript : MonoBehaviour
         {
             _tmpText.text = dialogues.dialogue[dialogueIndex];
         }
-
     }
-    
 }
