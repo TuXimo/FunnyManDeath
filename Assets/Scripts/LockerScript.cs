a60288ca58319f71f4600085ebba7ec1657a4829
Máximo Brunetti <67173706+TuXimo@users.noreply.github.com>
2024-01-28 11:20:06 -0300

    casi listo

---
+++ Assets/Scripts/LockerScript.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class LockerScript : MonoBehaviour
+{
+    [SerializeField] TMP_Text[] numberTexts = new TMP_Text[4];
+    [SerializeField] string codePassword = "1986";
+
+
+    public void SumNumber(int slot)
+    {
+        if (IsAcerted()) return;
+        int val0 = int.Parse(numberTexts[slot].text);
+        
+        if (Int32.Parse(numberTexts[slot].text) == 9)
+        {
+            numberTexts[slot].text = "0";
+        }
+        
+        else numberTexts[slot].text = (val0 + 1).ToString();
+
+        IsAcerted();
+    }
+    public void RestNumber(int slot)
+    {
+        if (IsAcerted()) return;
+        int val0 = int.Parse(numberTexts[slot].text);
+        
+        if (int.Parse(numberTexts[slot].text) == 0)
+        {
+            numberTexts[slot].text = "9";
+        }
+        
+        else numberTexts[slot].text = (val0 - 1).ToString();
+        IsAcerted();
+    }
+    public bool IsAcerted()
+    {
+        var currentLockCode = "";
+        
+        for (var i = 0; i < 4; i++)
+        {
+            currentLockCode += numberTexts[i].text;
+        }
+        
+        return currentLockCode == codePassword;
+    }
+
+    public void GetBackLockButton(GameObject currentLock)
+    {
+        if (IsAcerted())
+        {
+            closedChests.SetActive(false);
+            lockpad.SetActive(false);
+            openedChest.SetActive(true);
+        }
+
+        gameObject.SetActive(false);       
+    }
+
+    [SerializeField] private GameObject closedChests;
+    [SerializeField] private GameObject lockpad;
+    [SerializeField] private GameObject openedChest;
+    
+}
