1f7a4d27fcccb7839887369358d0e0a85f63a558
Máximo Brunetti <67173706+TuXimo@users.noreply.github.com>
2024-01-28 09:14:19 -0300

    pre candado

--- Assets/Scripts/ObjectButton.cs
+++ Assets/Scripts/ObjectButton.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
 
-public class ObjectButton : MonoBehaviour
+public class ObjectButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public GameObject objectReference;
     public Button button;
+
+    [SerializeField] private Texture2D hoverMouse, normalMouse;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Mouse is over GameObject.");
+        //Cursor.SetCursor(hoverMouse,Vector2.zero, cursorMode);
+    }
     
-    
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Mouse is over GameObject.");
+        //Cursor.SetCursor(normalMouse,Vector2.zero, cursorMode);
+    }
 }
--- Assets/Scripts/SceneManagerScript.cs
+++ Assets/Scripts/SceneManagerScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class SceneManagerScript : MonoBehaviour
@@ -8,5 +9,27 @@ public class SceneManagerScript : MonoBehaviour
     public void SetNextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    public void PauseGame ()
+    {
+        Time.timeScale = 0f;
+    }
+    public void ResumeGame ()
+    {
+        Time.timeScale = 1;
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+    [SerializeField] private AudioMixer _audioMixer;
+    
+    public void SetVolume(float volume)
+    {
+        _audioMixer.SetFloat("volume", volume);
     }
 }
